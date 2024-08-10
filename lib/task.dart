7ab498aa53846b37ac738c2c949669a0d8b6bd8d
Saliou <saliouseck2009@gmail.com>
2024-08-10 22:17:46 +0000

    refactor app

--- lib/task.dart
+++ lib/task.dart
@@ -1,6 +1,3 @@
-
-import 'package:flutter/material.dart';
-
 class Task {
   final String title;
   final String date;
@@ -9,17 +6,21 @@ class Task {
 }
 
 var sampleTask = [
-
-   Task(title: 'Sport', date: '14 Nov', ),
-   Task( title: 'Etude',date: '21 Nov',),
-                Task(title: 'Réunion', date: '12 Dec'),
-                Task(title: 'Webinaire sur les Soft Skills', date: '15 Dec'),
-                Task(title: "Passport de l'emploi", date: '18 Dec'),
-                Task(title: 'Entretien Stage', date: '19 Dec'),
-                Task(title: 'Soirée entre amis', date: '28 Dec'),
-                Task(title: 'Voyage ziguinchor', date: '29 Dec'),
-                Task(title: 'Diner familial', date: '31 Dec'),
-                Task(title: 'Anniversaire Mme ', date: '03 Jan'),
-                Task(title: 'Graduation Master', date: '16 Jan'),
-
+  Task(
+    title: 'Sport',
+    date: '14 Nov',
+  ),
+  Task(
+    title: 'Etude',
+    date: '21 Nov',
+  ),
+  Task(title: 'Réunion', date: '12 Dec'),
+  Task(title: 'Webinaire sur les Soft Skills', date: '15 Dec'),
+  Task(title: "Passport de l'emploi", date: '18 Dec'),
+  Task(title: 'Entretien Stage', date: '19 Dec'),
+  Task(title: 'Soirée entre amis', date: '28 Dec'),
+  Task(title: 'Voyage ziguinchor', date: '29 Dec'),
+  Task(title: 'Diner familial', date: '31 Dec'),
+  Task(title: 'Anniversaire Mme ', date: '03 Jan'),
+  Task(title: 'Graduation Master', date: '16 Jan'),
 ];
